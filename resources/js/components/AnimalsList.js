f5be971c8d10f4b3132a9107153de9e5e2d9d557
fouadmekhazni <fouadmekhazni7@gmail.com>
2022-03-09 15:24:30 +0100

    9-c ajouter méthode componentDidMount au composant AnimalsList

--- resources/js/components/AnimalsList.js
+++ resources/js/components/AnimalsList.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Animal from 'Animal';
+import axios from 'axios';
 
 class AnimalsList extends React.Component{
     constructor(){
         this.state={animals:[]}
+    }
+    componentDidMount(){
+        axios.get("/animals").then((res)=>{
+             this.setState(this.state.animals = res.data);
+        })
     }
     render(){
         return <div><Animal props={this.state.animals}></Animal></div>
